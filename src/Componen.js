80fb48ad39a78c6d24e51ab2d360af0289033975
viddushiva <viddushiva1@gmail.com>
2022-04-26 09:53:50 +0530

    s

--- src/Componen.js
+++ src/Componen.js
@@ -1,13 +1,26 @@
 import React, { useState } from "react"
 function Componen (props){
- const [count,setCount] = useState(0)
+ const [count,setCount] = useState("a")
+ const [coun,setCoun] = useState({name:"shiva",age:999})
+
   const change=()=>{
-setCount(count+1)
+setCount("b")
+  }
+  const chang=()=>{
+      setCoun((qw)=>({
+          ...qw,
+          age:coun.age+1
+      }))
   }
     return(
         <>
-        hello my name is {props.custom}{count}<br/>
+         hello my name is {props.custom}{count}<br/> 
+        hello my name is {coun.age}<br/>
+
+ 
         <button onClick={change}>functi click</button><br/>
+        <button onClick={chang}>functi click</button><br/>
+
         </>
     )
 }
